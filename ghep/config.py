94fa4281dcc5592e01dd43e9416f46febe078822
botrecon99 <conganh28999@gmail.com>
2025-05-24 16:22:12 +0700

    init

--- ghep/config.py
+++
@@ -1,9 +0,0 @@
-# config.py - Chứa cấu hình chính
-INPUT_FOLDER = "input_videos"  # Thư mục chứa video đầu vào
-OUTPUT_FOLDER = "output_videos"  # Thư mục chứa video sau khi ghép
-LOG_FOLDER = "logs"  # Thư mục chứa file log
-
-MAX_DURATION = 4200  # Thời lượng tối đa của mỗi video ghép (giây)
-TIME_FILE = f"{LOG_FOLDER}/time.txt"
-VIDEO_LIST_FILE = f"{LOG_FOLDER}/video_list.txt"
-USED_FIRST_VIDEOS_FILE = f"{LOG_FOLDER}/used_first_videos.txt"
